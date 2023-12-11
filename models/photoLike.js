6990ed083a9ea0472eea0fb27a8774c48671b452
Pogod171 <127141703+Pogod171@users.noreply.github.com>
2023-12-11 08:47:18 -0500

    Create photoLike.js

---
+++ models/photoLike.js
@@ -0,0 +1,23 @@
+import Model from './model.js';
+import UserModel from './user.js';
+import Repository from '../models/repository.js';
+
+export default class PhotoLike extends Model {
+    constructor()
+    {
+        super();
+        this.addField('OwnerId', 'string');
+        this.addField('Image', 'asset');
+        this.addField('IsLiked','boolean');
+
+        // this.setKey("Title");
+    }
+
+    bindExtraData(instance) {
+        instance = super.bindExtraData(instance);
+        let usersRepository = new Repository(new UserModel());
+        instance.Owner = usersRepository.get(instance.OwnerId);
+        instance.OwnerName = instance.Owner.Name;
+        return instance;
+    }
+}
